8124e9a651e64b2c646c6ef00caa85a9a06e8a72
Vladislav <melnikow.va@gmail.com>
2020-11-14 16:15:22 +0500

    added refresh and connect btn

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,18 +31,21 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.conBtn = new System.Windows.Forms.Button();
+            this.refreshBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(434, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(454, 13);
+            this.button1.Location = new System.Drawing.Point(452, 40);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -52,17 +55,49 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(13, 40);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 97);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(434, 324);
+            this.richTextBox1.Size = new System.Drawing.Size(434, 266);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // conBtn
+            // 
+            this.conBtn.BackColor = System.Drawing.Color.Lime;
+            this.conBtn.Location = new System.Drawing.Point(139, 13);
+            this.conBtn.Name = "conBtn";
+            this.conBtn.Size = new System.Drawing.Size(75, 23);
+            this.conBtn.TabIndex = 4;
+            this.conBtn.Text = "Connect";
+            this.conBtn.UseVisualStyleBackColor = false;
+            this.conBtn.Click += new System.EventHandler(this.conBtn_Click);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(220, 13);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshBtn.TabIndex = 5;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 376);
+            this.ClientSize = new System.Drawing.Size(675, 402);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.conBtn);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -78,6 +113,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button conBtn;
+        private System.Windows.Forms.Button refreshBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,11 +19,8 @@ namespace GRBL_CTRL
         public Form1()
         {
             InitializeComponent();
-
             port = new SerialPort();
-            port.PortName = "COM3";
             port.BaudRate = 115200;
-            port.Open();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,6 +28,37 @@ namespace GRBL_CTRL
             port.WriteLine(textBox1.Text);
             Thread.Sleep(200);
             richTextBox1.Text += port.ReadExisting();
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            comboBox1.Items.Clear();
+            foreach (var port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+        bool isConnected = false;
+        private void conBtn_Click(object sender, EventArgs e)
+        {
+            if(isConnected == true) //disconnect
+            {
+                port.Close();
+                conBtn.Text = "Connect";
+                conBtn.BackColor = Color.Green;
+            } else //connect
+            {
+                if (comboBox1.SelectedItem.ToString() != "")
+                {
+                    port.PortName = comboBox1.SelectedItem.ToString();
+                    port.Open();
+                }
+                conBtn.Text = "Disconnect";
+                conBtn.BackColor = Color.Red;
+            }
+            isConnected = !isConnected;
         }
     }
 }
